ad05a47274316170e077749d8c19d51f95aa0c6d
vbarkman <vincentbarkmann@gmail.com>
2019-07-06 14:39:07 +0200

    updated project to 2018.a.3f1, TextureImportHelper fix the sprites bleeding

---
+++ Pokemon Unity/Assets/Editor/TextureImportHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TextureImportHelper : AssetPostprocessor
+{
+    /// <summary>
+    /// Plugs in after a texture has been imported to change the default unity import settings
+    /// </summary>
+    /// <param name="texture"></param>
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        TextureImporter importer = assetImporter as TextureImporter;
+
+        importer.compressionQuality = 0;
+        importer.textureCompression = TextureImporterCompression.Uncompressed;
+        importer.filterMode = FilterMode.Point;
+        //uses the largest dimension of the texture to always have the optimal max size
+        importer.maxTextureSize = (texture.width > texture.height) ? texture.width : texture.height;
+        //Debug.Log($"Imported:{assetPath}");
+    }
+}
